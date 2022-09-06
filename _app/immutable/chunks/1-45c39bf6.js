0f13098d52478215c6a4c0a173abae08dccfe8b8
Matthew Valentine <2508029+matthewvalentine@users.noreply.github.com>
2022-09-06 01:37:38 -0700

    Try dumping a sveltekit site into gh pages

---
+++ _app/immutable/chunks/1-45c39bf6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-39232098.js";export{t as component};
---
+++ _app/immutable/chunks/2-96d623d9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-bb6189c5.js";export{t as component};
